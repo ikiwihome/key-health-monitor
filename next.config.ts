4a04daa85f356bd9a5b2d0e73036cdb644ed2ce5
ikiwi <kiwi@ikiwi.cc>
2026-01-28 16:14:13 +0800

    为docker构建添加standalone模式

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 移除 'export' 配置以支持 API 路由
-  // output: 'export',
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
